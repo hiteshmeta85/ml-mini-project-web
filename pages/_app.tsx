21904d97e8fd5b8f5aabe3e340baa71d991dc3b8
Hitesh Meta <meta.hitesh85@gmail.com>
2022-10-06 22:49:04 +0530

    Add NProgress Lib and ProgressBar component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import "@fontsource/ubuntu/400.css";
+import dynamic from "next/dynamic";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// nprogress
+const ProgressBar = dynamic(() => import('../components/ProgressBar/ProgressBar'), { ssr: false });
+
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <div>
+      <ProgressBar />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
