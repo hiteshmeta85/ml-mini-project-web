9c9233bce161c768c0745003c476b7b5f2a414af
Hitesh Meta <meta.hitesh85@gmail.com>
2022-10-07 01:30:30 +0530

    Make Home Page Responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Card = (props: Props) => {
   const {title, value} = props
 
   return (
-    <div className={'border border-2 border-transparent hover:border-white cursor-pointer text-center text-xl p-12 bg-[#2D3039] rounded-lg transition-all duration-200 hover:scale-105'}>
+    <div className={'border border-2 border-transparent hover:border-white cursor-pointer text-center text-xl px-6 py-8 md:p-12 bg-[#2D3039] rounded-lg transition-all duration-200 hover:scale-105'}>
       <p>{title}</p>
     </div>
   )
@@ -19,23 +19,21 @@ const Card = (props: Props) => {
 
 const Home: NextPage = () => {
   return (
-    <div className={'min-h-screen bg-[#1F2028] text-white relative'}>
-      <div className={'max-w-screen-2xl'}>
+    <div className={'bg-[#1F2028] text-white'}>
+      <div className={'max-w-screen-2xl relative'}>
         <div className={'text-right p-4 absolute top-0 right-0 z-10'}>
           <Link href={'/methodology'} passHref>
             <a>Methodology</a>
           </Link>
         </div>
-        <div className={'h-screen text-center'}>
-          <div className={'text-xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'}>
-            <div className={'mb-4'}>Select Disaster Type</div>
-            <div className={'grid grid-cols-3 gap-4'}>
-              {disasters.map((item, index)=> {
-                return (
-                  <Card key={index} title={item.title} value={item.value}/>
-                )
-              })}
-            </div>
+        <div className={'min-h-screen flex items-center justify-center flex-col text-center py-16 lg:py-0 text-xl'}>
+          <div className={'mb-4'}>Select Disaster Type</div>
+          <div className={'grid grid-cols-2 md:grid-cols-3 gap-4 mx-4'}>
+            {disasters.map((item, index)=> {
+              return (
+                <Card key={index} title={item.title} value={item.value}/>
+              )
+            })}
           </div>
         </div>
       </div>
