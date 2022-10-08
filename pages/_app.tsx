756ede627ff9a3437a27454bb5a25353422fe201
Hitesh Meta <meta.hitesh85@gmail.com>
2022-10-08 18:04:29 +0530

    Add Head Tag to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type {AppProps} from 'next/app'
 import "@fontsource/ubuntu/400.css";
 import dynamic from "next/dynamic";
+import Head from 'next/head';
 
 // nprogress
 const ProgressBar = dynamic(() => import('../components/ProgressBar/ProgressBar'), { ssr: false });
@@ -9,6 +10,10 @@ const ProgressBar = dynamic(() => import('../components/ProgressBar/ProgressBar'
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <div>
+      <Head>
+        <title>ML Mini Project</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      </Head>
       <ProgressBar />
       <Component {...pageProps} />
     </div>
